f35ff7e04d51da6f2d1b1eee7aae480cc5fc5a89
mil <milomory@gmail.com>
2023-05-01 18:03:41 +0300

    was added dotenv

--- app/settings.js
+++ app/settings.js
@@ -2,8 +2,10 @@ console.log('Settings start');
 
 const { Spot } = require('@binance/connector')
 
-const apiKey = 'U9OCcg0UWiEITdbwzD9JXQVs0KP6tPFnfU1YoW54FFbhf0fuUDm9Jc4YfCR7Xgwg'
-const apiSecret = 'Z8wOuZ3OnvEEqC45aSRuMwv3adeMRJXlx8D028Ln2pCkXO4lE6AVFbwkHnAuGcNE'
+require('dotenv').config();
+const apiKey = process.env.apiKey
+const apiSecret = process.env.apiSecret
+
 
 exports.getSettings = () => {
     return new Spot(apiKey, apiSecret)
